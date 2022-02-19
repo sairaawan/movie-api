ac431c9a5c956553ad4d1491569eb42f5631cf7f
sairaawan <syraawan@live.com>
2022-02-19 20:26:17 +0000

    added fetch methods to get movie by year and rating

--- src/main/java/com/ssp/movie/api/service/MovieService.java
+++ src/main/java/com/ssp/movie/api/service/MovieService.java
@@ -1,4 +1,15 @@
 package com.ssp.movie.api.service;
 
+import com.ssp.movie.api.entity.Movie;
+
+import java.util.List;
+
 public interface MovieService {
+   public  List<Movie> fetchMoviesListByRatingSorted(int page);
+
+  public Movie saveMovie(Movie movie);
+
+   public  List<Movie> fetchMoviesListByRatings(int rating);
+
+   public List<Movie> fetchMoviesListByYear(int year);
 }
--- src/main/java/com/ssp/movie/api/service/MovieServiceImpl.java
+++ src/main/java/com/ssp/movie/api/service/MovieServiceImpl.java
@@ -1,8 +1,42 @@
 package com.ssp.movie.api.service;
 
+import com.ssp.movie.api.entity.Movie;
+import com.ssp.movie.api.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 
 @Service
 public class MovieServiceImpl implements MovieService{
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    @Override
+    public List<Movie>fetchMoviesListByRatingSorted(int page) {
+        Pageable sortByRating= PageRequest.of(page,3, Sort.by("rating"));
+        List<Movie> movies
+                = movieRepository.findAll(sortByRating).getContent();
+       return movies;
+
+    }
+
+    @Override
+    public Movie saveMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @Override
+    public List<Movie> fetchMoviesListByRatings(int rating) {
+        return movieRepository.getMovieByRating(rating);
+    }
+
+    @Override
+    public List<Movie> fetchMoviesListByYear(int year) {
+        return movieRepository.findByFirstYear(year);
+    }
 }
