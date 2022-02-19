7f7c80c067428db374b67f413b0221772b67d4ba
sairaawan <syraawan@live.com>
2022-02-19 19:44:07 +0000

    updated return movies by year method

--- src/main/java/com/ssp/movie/api/repository/MovieRepository.java
+++ src/main/java/com/ssp/movie/api/repository/MovieRepository.java
@@ -3,6 +3,7 @@ package com.ssp.movie.api.repository;
 import com.ssp.movie.api.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,8 +11,10 @@ import java.util.List;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    List<Movie> findByYear(int year);
-
     @Query("select s from Movie s where s.rating= ?1")
     List<Movie> getMovieByRating(int rating);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM Movie s WHERE s.year = :year ORDER BY rating DESC LIMIT 3")
+    List<Movie> findByYear(@Param("year") int year);
+
 }
