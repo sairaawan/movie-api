4abd1ef8219a5a668dc0199b79d97eccba554873
sairaawan <syraawan@live.com>
2022-02-19 20:26:49 +0000

    added custom queries

--- src/main/java/com/ssp/movie/api/repository/MovieRepository.java
+++ src/main/java/com/ssp/movie/api/repository/MovieRepository.java
@@ -2,8 +2,16 @@ package com.ssp.movie.api.repository;
 
 import com.ssp.movie.api.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findByFirstYear(int year);
+
+    @Query("select s from Movies s where s.ratings= ?1")
+    List<Movie> getMovieByRating(int rating);
 }
