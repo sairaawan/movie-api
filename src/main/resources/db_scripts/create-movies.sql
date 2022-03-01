7fc00835e6d1528cf28ef25afad65c7b86bab527
Paul Robertson <paul.robertson999@googlemail.com>
2022-03-01 21:08:24 +0000

    Making collate on columns case-insensitive

--- src/main/resources/db_scripts/create-movies.sql
+++ src/main/resources/db_scripts/create-movies.sql
@@ -3,16 +3,16 @@ USE imdb;
 DROP TABLE IF EXISTS Movie;
 
 CREATE TABLE  Movie (
-  `movieId` varchar(20) COLLATE utf8mb4_bin NOT NULL,
-  `movieType` varchar(20) COLLATE utf8mb4_bin DEFAULT NULL,
-  `movieName` text COLLATE utf8mb4_bin,
-  `originalTitle` text COLLATE utf8mb4_bin,
+  `movieId` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `movieType` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `movieName` varchar(255) COLLATE utf8mb4_general_ci,
+  `originalTitle` text COLLATE utf8mb4_general_ci,
   `isAdult` tinyint(1) DEFAULT NULL,
   `releaseYear` int(4) unsigned DEFAULT NULL,
   `endYear` int(4) unsigned DEFAULT NULL,
   `runningTime` int(4) unsigned DEFAULT NULL,
-  `movieGenre` varchar(80) COLLATE utf8mb4_bin DEFAULT NULL, `averageRating` int(4) unsigned DEFAULT NULL,`numberOfVotes` int(4) unsigned DEFAULT NULL, PRIMARY KEY (`movieId`) 
-)   ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+  `movieGenre` varchar(80) COLLATE utf8mb4_general_ci DEFAULT NULL, `averageRating` int(4) unsigned DEFAULT NULL,`numberOfVotes` int(4) unsigned DEFAULT NULL, PRIMARY KEY (`movieId`)
+)   ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOAD DATA LOCAL INFILE 'titles.tsv'
 INTO TABLE Movie
--- src/main/resources/db_scripts/create-ratings.sql
+++ src/main/resources/db_scripts/create-ratings.sql
@@ -2,10 +2,10 @@ USE imdb;
 
 DROP TABLE IF EXISTS movie_ratings;
 CREATE TABLE  movie_ratings (
-  `movieId` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `movieId` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
   `averageRating` float DEFAULT NULL,
   `numberOfVotes` int(11) DEFAULT NULL
-, PRIMARY KEY (`movieId`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+, PRIMARY KEY (`movieId`) ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 LOAD DATA LOCAL INFILE  'ratings.tsv'
 INTO TABLE movie_ratings
